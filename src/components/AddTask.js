02b483a7cd97bf2597a898eb3e912253568ff286
Dipesh-Gyawali <dipeshgyawali365@gmail.com>
2023-05-09 15:01:45 +0545

    addtask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -6,16 +6,21 @@ export const AddTask = () => {
     const handleClear = () => {
         setWord(" ");
     }
+    const countWords = (text) => {
+        const breakWord = text.trim().split(/\s+/);
+        return breakWord.length;
+      }
+
     return (
         <div>
-            <h1>Word Counte</h1>
+            <h1>Word Counter</h1>
             <p>Free online character and word count tool</p>
             <textarea type="text" value={word} placeholder='Type or paste your text' onChange={(e) => setWord(e.target.value)} ></textarea>
             <br/>
             <button onClick={handleClear}>Clear</button>
             <br/>
-            <button>Character</button>
-            <button>Word</button>
+            <button>Character Length {word.length}</button>
+            <button>Number of words: {countWords(word)}</button>
         </div>
     )
     }
---
+++ src/components/Jokes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Jokes = () => {
+  return (
+    <div>
+        <h1>ChuckNorris Jokes</h1>
+    </div>
+  )
+}

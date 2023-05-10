abf60f96d10f669e3418d997585208178a6a20fd
Dipesh-Gyawali <dipeshgyawali365@gmail.com>
2023-05-10 18:24:18 +0545

    Logic completed

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,27 +1,32 @@
 import React, { useState } from 'react'
-
+import "../App.css"
 export const AddTask = () => {
     const [word,setWord] = useState("");
 
     const handleClear = () => {
         setWord(" ");
     }
+    
     const countWords = (text) => {
+        if (text.trim() === "") {
+            return 0;
+        }
         const breakWord = text.trim().split(/\s+/);
         return breakWord.length;
       }
 
-    return (
-        <div>
+      return (
+        <div className='addtask'>
             <h1>Word Counter</h1>
             <p>Free online character and word count tool</p>
             <textarea type="text" value={word} placeholder='Type or paste your text' onChange={(e) => setWord(e.target.value)} ></textarea>
             <br/>
             <button onClick={handleClear}>Clear</button>
             <br/>
-            <button>Character Length {word.length}</button>
-            <button>Number of words: {countWords(word)}</button>
-            <hr/>
+            <div className='btns'>
+                <span>Character Length {word.length}</span>
+                <span>Number of words: {countWords(word)}</span>
+            </div>
         </div>
     )
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+      <header>
+        <nav>
+          <ul>
+            <li><NavLink to="/" end>Home</NavLink></li>
+            <li><NavLink to="/jokes">Jokes</NavLink></li>
+          </ul>
+        </nav>
+      </header>
   )
 }

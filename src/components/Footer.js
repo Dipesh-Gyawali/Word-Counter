d9496d0e992359d44447a14700cb884fb982c4de
Dipesh-Gyawali <dipeshgyawali365@gmail.com>
2023-05-11 16:13:35 +0545

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+    <div class="footer-content">
+      <p>Phone: <span id="phone-number">+977-9840031045</span></p>
+    </div>
+</footer>
+
   )
 }
